f62819b15305d1cb2f26cad891e90375f27949b3
Evan Carlson <evanrc1@gmail.com>
2024-02-19 21:44:10 -0800

    created routes directory with all files, and then filled out userRoutes.js

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+const express = require('express');
+const app = express();
 const mongoose = require('mongoose'); // require mongoose
+const { User, Thought } = require('./models');
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
 
 // Defined the mongoDB URI (Uniform Resoure Identifer).
 const mongoDBUri = 'mongodb://localhost:27017'; // This URI specifies the location of the MongoDB server, in this case being the default port of (297017)
-
 mongoose.connect(mongoDBUri, // Connects to the MongoDb using the mongoose.connct()
      { useNewUrlParser: true, // Tells Mogoose to use the new URl parser instead of the depricated one.
         useUnifiedTopology: true }) // opts in to using the MongoDB drivers new connection managment engine.
     
 .then(() => console.log('Successfully connected to MongoDB!')) // This promis resolves if the connection is successful.
 .catch(err => console.error('There was a connection error', err)); // If an error occurs, logs the error message to the console.
+
+app.use(express.json()); // for parsing application/json
+app.use(userRoutes); // Use the user routes
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
---
+++ routes/userRoutes.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+const { User } = require('../models/user')
+
+// Get all the users
+router.get('./api/users', async (req, res) => {
+    try {
+        const users = await User.find().select('-_ _v'); // Exclude the version key from the result
+        res.json(users);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+})
+
+// Get a single User by it's ID
+router.get('/api/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+            .select('-_ _v')
+            .populate('thoughts')
+            .populate('friends');
+        if (!user) {
+            return res.status(404).json({ message: 'No user found with this id!' });
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+// Create a new user
+router.post('/api/users', async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+        const user = await newUser.save();
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+});
+
+// Update a user by _id
+router.put('/api/users/:id', async (req, res) => {
+    try {
+        const user = await user.findByIdAndUpdate(req.params.id, req.body,
+            { new: true, runValidators: true });
+            if(!user) {
+                return res.status(404).json({ message: 'No user found with this ID!' });
+            } 
+            res.json(user);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+});
+
+// Delete a user by _id
+router.delete('/api/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: ' No user found with this id!' });
+        }
+        res.json({ message: 'User was successfully Terminated!' });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
